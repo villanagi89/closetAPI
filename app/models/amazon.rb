d6ff527856de008a1b7669bd72b6769e46c1a1bc
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-04-08 12:04:05 -0400

    updated amazon model

--- app/models/amazon.rb
+++ app/models/amazon.rb
@@ -5,11 +5,11 @@ class Amazon
       "{'expiration': '#{30.minutes.from_now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z')}',
       'conditions': [
         {'bucket': '#{ENV['S3_BUCKET']}'},
-        ['starts-with', '$key', '#{@key}'],
+        ['starts-with', '$key', '#{key}'],
         ['starts-with', '$Content-Type', 'image/jpeg'],
         ['content-length-range', 0, 10485760],
       ]}").gsub(/\n|\r/, '')
     signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha1'), ENV['AWS_SECRET_ACCESS_KEY'], policy)).gsub(/\n| |\r/, '')
-    return {access_key: ENV['AWS_ACCESS_KEY_ID'], key: key, policy: policy, signature: signature}
+    return {access_key: ENV['AWS_ACCESS_KEY_ID'], key: key, policy: policy, signature: signature, bucket: ENV['S3_BUCKET']}
   end
 end
